f04b26f8d8fbe070bfc05133435bcde7e196c306
Edouard Herrengt <herrengt.edouard@gmail.com>
2022-02-06 17:01:32 +0100

    wip

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 const btn = document.querySelector("#btn");
-const modal = document.querySelector("#modal");
+const modal = document.querySelector(".modal");
 
 btn.addEventListener("click", (e) => {
-  modal.classList.add("active");
-  if (e.target.id != modal) {
-    modal.classList.remove("active");
-  }
+  modal.classList.toggle("active");
 });
